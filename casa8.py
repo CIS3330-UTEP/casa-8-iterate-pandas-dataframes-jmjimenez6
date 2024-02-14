5cbb7e6580fa9f924825b4d03f88b70a1a40f754
jmjimenez6 <jmjimenez6@miners.utep.edu>
2024-02-13 21:39:51 -0700

    Create casa8.py

---
+++ casa8.py
@@ -0,0 +1,19 @@
+import pandas
+
+list_teams =[' 49ers', 'KC', 'Cowboys', 'Steelers']
+
+print(type(list_teams))
+
+print(list_teams)
+
+series_teams = pandas.Series(list_teams)
+
+print(series_teams)
+print(type(series_teams))
+
+
+students = {' hawaii': 'Isebella','Ohio': }
+
+df = pandas.read_csv('big-mac-fullindex.csv')
+
+print(type(df))
